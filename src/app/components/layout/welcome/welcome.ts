88bac8bb0ac747cd13282bd9cc79f009260d53a4
Tamilselvan <tamilselvan@aggrandizeventure.com>
2026-08-11 19:36:24 +0800

    welcome page design added

--- src/app/components/layout/welcome/welcome.ts
+++ src/app/components/layout/welcome/welcome.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,10 +6,54 @@ import { Router } from '@angular/router';
   templateUrl: './welcome.html',
   styleUrls: ['./welcome.scss'],
 })
-export class WelcomeComponent {
-  constructor(private router: Router) {}
+export class WelcomeComponent implements OnInit, OnDestroy {
+  todayDate: string = '';
+  currentTime: string = '';
+  private timer: any;
 
-  navigateToDashboard(): void {
-    // this.router.navigate(['/dashboard']);
+  constructor(
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.updateDateTime();
+
+    this.timer = setInterval(() => {
+      this.updateDateTime();
+      this.cdr.detectChanges();
+    }, 1000);
+  }
+
+  updateDateTime(): void {
+    const now = new Date();
+
+    this.todayDate = now.toLocaleDateString('en-IN', {
+      weekday: 'long',
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    });
+
+    this.currentTime = now.toLocaleTimeString('en-IN', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  onViewReports(): void {
+    this.router.navigate(['/main/dashboard']);
+  }
+
+  onCreateReceipt(): void {
+    this.router.navigate(['/main/new-receipt']);
   }
 }
